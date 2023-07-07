39577640bb7bf382a7b8508ca62f1d34457a512c
Vikas <vikask4590@gmail.com>
2023-07-08 00:30:58 +0530

    chat app in php

---
+++ php/get-chat.php
@@ -0,0 +1,52 @@
+<?php 
+    session_start();
+    if(isset($_SESSION['unique_id'])){
+        include_once "config.php";
+        $outgoing_id = $_SESSION['unique_id'];
+        $incoming_id = mysqli_real_escape_string($conn, $_POST['incoming_id']);
+        $output = "";
+        $sql = "SELECT * FROM messages LEFT JOIN users ON users.unique_id = messages.outgoing_msg_id
+                WHERE (outgoing_msg_id = {$outgoing_id} AND incoming_msg_id = {$incoming_id})
+                OR (outgoing_msg_id = {$incoming_id} AND incoming_msg_id = {$outgoing_id}) ORDER BY msg_id";
+        $query = mysqli_query($conn, $sql);
+        if(mysqli_num_rows($query) > 0){
+            while($row = mysqli_fetch_assoc($query)){
+                if($row['outgoing_msg_id'] === $outgoing_id){
+                    $output .= '<div class="chat outgoing">
+                                <div class="details">
+                                    <p>'. $row['msg'] .'</p>
+                                </div>
+                                </div>
+                                <p class="time">'.$row['date'].'</p>';
+                }else{
+                    $output .= '<div class="chat incoming">
+                                <img src="php/images/'.$row['img'].'" alt="">
+                                <div class="details">
+                                    <p>'. $row['msg'] .'</p>
+                                </div>
+                                </div>
+                                <p class="i-time">'.$row['date'].'</p>';
+                }
+            }
+        }else{
+            $output .= '<div class="text">No messages are available. Once you send message they will appear here.</div>';
+        }
+        echo $output;
+    }else{
+        header("location: ../login.php");
+    }
+
+?>
+<style>
+    .time{
+        text-align: right;
+        font-size: 8px;
+        margin-top: -15px;
+    }
+    .i-time{
+        text-align: left;
+        margin-left: 46px;
+        font-size: 8px;
+        margin-top: -15px;
+    }
+</style>
---
+++ php/insert-chat.php
@@ -0,0 +1,20 @@
+<?php 
+    session_start();
+    date_default_timezone_set('Asia/Kolkata');
+    if(isset($_SESSION['unique_id'])){
+        include_once "config.php";
+        $outgoing_id = $_SESSION['unique_id'];
+        $incoming_id = mysqli_real_escape_string($conn, $_POST['incoming_id']);
+        $message = mysqli_real_escape_string($conn, $_POST['message']);
+        if(!empty($message)){
+            $date = date("h:i:s");
+            $sql = mysqli_query($conn, "INSERT INTO `messages`(`msg_id`, `incoming_msg_id`, `outgoing_msg_id`, `msg`, `date`)
+                                        VALUES (NULL,{$incoming_id}, {$outgoing_id}, '{$message}','$date')") or die();
+        }
+    }else{
+        header("location: ../login.php");
+    }
+
+
+    
+?>
